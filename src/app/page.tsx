a6f34b9dd71396bd4b6582d78252fedcff2bd70a
AdityaWisnuu <adtywisnuwardana003@gmail.com>
2025-10-23 23:13:41 +0700

    feat: add farcaster sdk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
+"use client";
+
+import { sdk } from "@farcaster/miniapp-sdk";
+import { useEffect } from "react";
 import TrendingSection from "@/features/home/components/TrendingSection";
 import Image from "next/image";
 
 export default function Home() {
+  useEffect(() => {
+    sdk.actions.ready();
+  }, []);
   return (
     <div className="w-full h-screen bg-gradient-to-r from-orange-500 to-orange-400">
       <div className="w-full flex flex-col gap-2 p-8 text-white">
-        <Image src={'/logo.png'} alt="Fluxoria" width={256} height={45} />
+        <Image src={"/logo.png"} alt="Fluxoria" width={256} height={45} />
         <span className="font-semibold">Lets leverage your bets.</span>
       </div>
       <TrendingSection />
     </div>
-  )
+  );
 }
